d59a3fa5661a1cbe923c25395ab230c4b4d1526f
Jarvist Moore Frost <jarvist@gmail.com>
2023-11-03 11:36:10 +0000

    Energies to eV. Conversion to Ha more explicit.

--- 0004-Singlet-Fission/Grozema_PDI.jl
+++ 0004-Singlet-Fission/Grozema_PDI.jl
@@ -10,19 +10,21 @@ const invcm2au = 4.55633e-6
 const mev2invcm = 8.066
 const mev2au=mev2invcm*invcm2au
 
+const eV2Ha=1/27.2114079527
+
 const nm2au = 18.897
 
 const au2fs = 0.02418884254
 
 function pdi()
     println("Entering PDI routine...")
-    Es = 2.22 * 1000
-    Ec = 3.11 * 1000
-    Et =  1.05 * 1000
-    t2e = -0.42
-    thh = -125.0
-    tll = 145.0
-    thl = -125.0
+    Es = 2.22 
+    Ec = 3.11 
+    Et =  1.05 
+    t2e =   -0.42 / 1000
+    thh = -125.0  / 1000
+    tll =  145.0  / 1000
+    thl = -125.0  / 1000
 
     N = 5
 
@@ -32,17 +34,17 @@ function pdi()
         tll -thh Ec 0 √(3/2)*thl
         -thh tll 0 Ec √(3/2)*thl
         t2e t2e √(3/2)*thl √(3/2)*thl 2*Et
-    ]) * mev2au
-    println("H0 = ", H0)
+    ]) * eV2Ha
+    println("H0 (Ha) = ", H0)
 
-    Jw = SpectralDensities.DrudeLorentz(λ=135*mev2au, γ=150*mev2au, Δs=1.0)
+    Jw = SpectralDensities.DrudeLorentz(λ=135E-3*eV2Ha, γ=150E-3*eV2Ha, Δs=1.0)
     println("Jw = ", Jw)
 
     nsteps = 2000
     ρ0 = Matrix{ComplexF64}(zeros(N, N))
     ρ0[1, 1] = 1.0
 
-    T = 50.0:50.0:500.0
+    #T = 50.0:50.0:500.0
 
     β = 1 / (300 * 3.16683e-6) # JMF what units?
 
@@ -54,7 +56,7 @@ function pdi()
 
     hb = Solvents.HarmonicBath(β, ωs, cs, [1.0, 2.0, 3.0, 4.0, 5.0].*d, 1000)
     
-    println("hb = ", hb)
+    #println("hb = ", hb)
 
     println("Propagating...")
     tc, ρc = QCPI.propagate( 
