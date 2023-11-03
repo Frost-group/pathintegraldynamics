c155dcd5b6ddfc30f342678d7924bb79c548f35b
Jarvist Moore Frost <jarvist@gmail.com>
2023-11-03 11:19:38 +0000

    Clean up code; echo setup as constructed.

--- 0004-Singlet-Fission/Grozema_PDI.jl
+++ 0004-Singlet-Fission/Grozema_PDI.jl
@@ -3,15 +3,19 @@ using Plots
 using LinearAlgebra
 
 
-
+# Atomic units to SI units conversion
 const thz2au = 0.0001519828500716
+
 const invcm2au = 4.55633e-6
-const au2fs = 0.02418884254
 const mev2invcm = 8.066
+const mev2au=mev2invcm*invcm2au
+
 const nm2au = 18.897
 
+const au2fs = 0.02418884254
 
 function pdi()
+    println("Entering PDI routine...")
     Es = 2.22 * 1000
     Ec = 3.11 * 1000
     Et =  1.05 * 1000
@@ -28,9 +32,11 @@ function pdi()
         tll -thh Ec 0 √(3/2)*thl
         -thh tll 0 Ec √(3/2)*thl
         t2e t2e √(3/2)*thl √(3/2)*thl 2*Et
-    ]) * mev2invcm * invcm2au
+    ]) * mev2au
+    println("H0 = ", H0)
 
-    Jw = SpectralDensities.DrudeLorentz(λ=135*mev2invcm*invcm2au, γ=150*mev2invcm*invcm2au, Δs=1.0)
+    Jw = SpectralDensities.DrudeLorentz(λ=135*mev2au, γ=150*mev2au, Δs=1.0)
+    println("Jw = ", Jw)
 
     nsteps = 2000
     ρ0 = Matrix{ComplexF64}(zeros(N, N))
@@ -38,16 +44,31 @@ function pdi()
 
     T = 50.0:50.0:500.0
 
-    β = 1 / (T[6] * 3.16683e-6) # T = 300K
+    β = 1 / (300 * 3.16683e-6) # JMF what units?
 
     dt = 0.05/au2fs
 
-    d = 1
+    d = 1 # JMF: what this?
 
     ωs, cs = SpectralDensities.discretize(Jw, 100)
 
     hb = Solvents.HarmonicBath(β, ωs, cs, [1.0, 2.0, 3.0, 4.0, 5.0].*d, 1000)
-    tc, ρc = QCPI.propagate(; Hamiltonian=H0, Jw, solvent=hb, ρ0, classical_dt=dt, dt, ntimes=nsteps, kmax=1, svec=[1.0 2.0 3.0 4.0 5.0].*d, extraargs=QuAPI.QuAPIArgs(), path_integral_routine=QuAPI.propagate)
+    
+    println("hb = ", hb)
+
+    println("Propagating...")
+    tc, ρc = QCPI.propagate( 
+        ; Hamiltonian=H0, 
+        Jw, 
+        solvent=hb, 
+        ρ0, 
+        classical_dt=dt, dt, 
+        ntimes=nsteps, 
+        kmax=1, 
+        svec=[1.0 2.0 3.0 4.0 5.0].*d, 
+        extraargs=QuAPI.QuAPIArgs(), 
+        path_integral_routine=QuAPI.propagate)
+    println("Propagation done.")
 
     plot(tc.*au2fs, real.(ρc[:, 1, 1]), label="S1S0")
     plot!(tc.*au2fs, real.(ρc[:, 3, 3]), label="CT")
@@ -58,3 +79,4 @@ function pdi()
 end
 
 pdi()
+
