b8548e25d58650dfcc3d31bffbb8c51ee790b8bc
chemicalfiend <teamkagaku101@gmail.com>
2024-03-15 15:25:54 +0530

    Upconversion trimer

--- 0006-Y6-Charge-Transfer/Upconversion.jl
+++ 0006-Y6-Charge-Transfer/Upconversion.jl
@@ -23,29 +23,87 @@ function UpconversionHEOM(; dt=0.25/au2fs, nsteps=4000, L=5, K=2)
 
     # D1 dimer dynamics - FE vs CT character over time.
 
-    reorg = 157 * mev2au
+    #reorg = 157 * mev2au
 
-    cutoff = 1600 * invcm2au
+    #cutoff = 1600 * invcm2au
 
-    Efe = 2000.0
-    Ect(r) = (2.19 -  4.959/(r))*1000  # Enter r in angstrom ; best fit equation for Ect
-    Dh = 55.7
-    De = 72.0
-    V = -76.0
+    #Efe = 2000.0
+    #Ect(r) = (2.19 -  4.959/(r))*1000  # Enter r in angstrom ; best fit equation for Ect
+    #Dh = 55.7
+    #De = 72.0
+    #V = -76.0
     
-    Ec = Ect(9.29)
+    #Ec = Ect(9.29)
     
-    N = 4
+    #N = 4
 
 
+# Dimer Hamiltonian
 
+#=
     H0 = Matrix{ComplexF64}([    ## Check this pls
         Efe V Dh De
         V Efe De Dh
         Dh De Ec 0.0
         De Dh 0.0 Ec
     ]) * mev2au
+=#
 
+    # D1, D3, D8 trimer dynamics - FE vs CT character over time.
+
+   reorg = 157 * mev2au
+
+   cutoff = 1600 * invcm2au
+
+   Ef1 = 2046.0 # eV
+   Ef3 = 2046.0
+   Ef8 = 2046.0
+
+   Ect(r) = (2.19 -  4.959/(r))*1000  # Enter r in angstrom ; best fit equation for Ect
+   
+   r1 = 9.29
+   r3 = 13.84
+   r8 = 15.97
+
+   Dh1 = 55.7
+   Dh3 = -27.3
+   Dh8 = 0.0
+   De1 = 72.0
+   De3 = -15.0
+   De8 = 0.0
+   
+   V1 = -76.0
+   V3 = -6.1
+   V8 = -9.0
+   
+   Ec1 = Ect(r1) # 1656.2 eV
+   Ec3 = Ect(r3) # 1831.69 eV
+   Ec8 = Ect(r8) # 1879.48 eV
+   
+   te1 = 66.1
+   te3 = -27.0
+   te8 = 0.0
+   th1 = 49.6
+   th3 = -11.3
+   th8 = 0.0
+
+
+# Trimer Hamiltonian
+    
+    
+
+    H0 = Matrix{ComplexF64}([
+         Ef1 V1 V8 Dh1 Dh8 De1 0.0 De8 0.0
+         V1 Ef3 V3 De1 0.0 Dh1 Dh3 0.0 De3
+         V8 V3 Ef8 0.0 De8 0.0 De3 Dh8 Dh3
+         Dh1 De1 0.0 Ec1 th3 0.0 0.0 0.0 te8
+         Dh8 0.0 De8 th3 Ec8 0.0 te1 0.0 0.0  
+         De1 Dh1 0.0 0.0 0.0 Ec1 th8 te3 0.0
+         0.0 Dh3 De3 0.0 te1 th8 Ec3 0.0 0.0
+         De8 0.0 Dh8 0.0 0.0 te3 0.0 Ec8 th1
+         0.0 De3 Dh3 te8 0.0 0.0 0.0 th1 Ec3
+    ]) * mev2au
+    
     ρ0 = Matrix{ComplexF64}(zeros(N, N))
     ρ0[2, 2] = 1.0
     β = 1 / (300 * 3.16683e-6) # T = 300K
