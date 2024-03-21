b7c28316715654bf1bf86f2b03f41272ef363459
chemicalfiend <teamkagaku101@gmail.com>
2024-03-21 12:06:28 +0530

    500 Plots

--- 0006-Y6-Charge-Transfer/plot-routines.jl
+++ 0006-Y6-Charge-Transfer/plot-routines.jl
@@ -12,102 +12,15 @@ const mev2au = mev2invcm * invcm2au
 const nm2au = 18.897
 
 
-"""
-plot1DHolstein(N, a)
-
-Plot out the populations, MSD and dMSD/dt for an N-site 1D lattice with spacing a (nm).
-
-"""
-
-function plot1DHolstein(fname, a)
-    # Plot populations
-
-    dlm = readdlm(fname, Float64)
-    
-    t = dlm[:, 1].*au2fs
-    
-    ρs = []
-
-    N = size(dlm)[2] - 1
-
-    nsteps = size(dlm)[1]-1
-    
-    nsteps = 5000
-
-    @gp "set key left" "set logscale x"
-    @gp :- "set title 'Populations'"
-    for i in 2:size(dlm)[2]
-        ρ = dlm[:, i]
-        push!(ρs, ρ)
-        #plot!(t[2:nsteps], ρ[2:nsteps], label="site $(i-1)", xscale=:log10)
-        @gp :- t[2:nsteps] ρ[2:nsteps] "w l tit 'site $(i-1)' dt 1 lw 2"
-    end
-    #savefig("Populations.png")
-
-    Gnuplot.save("Populations.png", term="pngcairo size 550,350 fontscale 0.8")
-    
-    MSD = Vector{Float64}(undef, nsteps+1)
-    for i in 1:(nsteps+1)
-        s= 0.0
-        for j in 1:N
-            s += ρs[j][i]*((j-1)^2)*(a * nm2au)^2
-        end
-        MSD[i] = s
-    end
-
-    dMSD = [(MSD[i+1] - MSD[i-1])/(t[i+1] - t[i-1]) for i in 2:(nsteps)]
-
-    @gp "set key left" "set logscale x"
-    @gp :- "set title 'MSD'"
-
-    @gp :- t[2:nsteps-1] MSD[2:nsteps-1] "w l tit ' ' dt 1 lw 2"
-
-    Gnuplot.save("MSD.png", term="pngcairo size 550,350 fontscale 0.8")
-    #plot(t[2:nsteps], MSD[2:nsteps], xscale=:log10)
-
-    #savefig("MSD.png")
-
-
-    @gp "set key left" "set logscale x"
-    @gp :- "set title 'dMSD/dt'"
-
-    @gp :- t[2:nsteps-1] dMSD[2:nsteps-1] "w l tit 'dMSD/dt' dt 1 lw 2"
-
-    Gnuplot.save("dMSD_dt.png", term="pngcairo size 550,350 fontscale 0.8")
-    #plot(t[2:nsteps], MSD[2:nsteps], xscale=:log10)
-    #plot(t[2:nsteps-1], dMSD[2:nsteps-1].*(5), xscale=:log10)
-
-   # savefig("dMSD_dt.png")
-    
-   #=
-    # Moving average dMSD_dt
-    
-    n_bin = 45
-
-    mov = []
-    for i in 1:(length(enumerate(MSD))-n_bin)
-        sum = 0
-        for j in 1:n_bin
-            sum += MSD[i+j-1]
-        end
-        push!(mov, sum/(n_bin))
-    end
-    
-    dMSD_agg = [(MSD[i+1] - MSD[i-1])/(t[i+1] - t[i-1]) for i in 2:(nsteps-n_bin-1)]
-    
-    #plot(t[2:(nsteps-n_bin-1)], dMSD_agg, xscale=:log10)
-
-    savefig("dMSD_agg.png")
-    =#
-    
-end
-
-
 function UpconversionPlotDimer(fname)
     # Plot populations
 
     dlm = readdlm(fname, Float64)
     
+    tags = split(fname, "-")
+    s = parse(Float64, tags[6])
+    V = parse(Float64, tags[8])
+
     t = dlm[:, 1].*au2fs
     
     ρs = []
@@ -127,7 +40,7 @@ function UpconversionPlotDimer(fname)
     #plot!(t[2:nsteps], ρCT1[2:nsteps]+ρCT2[2:nsteps], label="CT")
     
     @gp "set key left"
-    @gp :- "set title 'Y6 Dimer Redfield (Free Parameters - Vct = 162meV, SoC = 71.0meV)'"
+    @gp :- "set title 'Y6 Dimer Redfield (Free Parameters - Vct = $V meV, SoC = $s meV)'"
     @gp :- "set xlabel 't(fs)'"
     @gp :- "set ylabel 'Population'"
     
@@ -137,7 +50,7 @@ function UpconversionPlotDimer(fname)
     @gp :- t[2:nsteps] ρCT1[2:nsteps] "w l tit 'CT1' dt 1 lw 2 lc rgb '#74C476' "
     @gp :- t[2:nsteps] ρCT2[2:nsteps] "w l tit 'CT2' dt 1 lw 2 lc rgb '#238B45' "
     
-    Gnuplot.save("Results/Y6-dimer-upconversion-populations-redfield.png", term="pngcairo size 550,350 fontscale 0.8")
+    Gnuplot.save("Y6-Plots/Y6-dimer-upconversion-populations-redfield-Vct-$V-SoC-$s.png", term="pngcairo size 550,350 fontscale 0.8")
 
     #savefig("Dimer-TTM-Populations.png")
 end
@@ -247,7 +160,9 @@ function UpconversionYieldDimer()
     
 end
 
-UpconversionPlotDimer("stdout-files5/upconversion-populations-brme-s-71.0-V-162.0-t-2000.0.stdout")
+for fname in readdir("stdout-files5")
+    UpconversionPlotDimer("stdout-files5/" * fname)
+end
 
 #UpconversionYieldDimer()
 # Used 0.5nm arbitrarily here, need to figure out site distances for real materials
